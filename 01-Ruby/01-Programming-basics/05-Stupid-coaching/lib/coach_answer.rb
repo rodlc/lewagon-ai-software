bfa77318a1b886eed28ef8b2b84bb2e46e5cdbbc
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-14 16:08:06 +0200

    Welcome coach

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,7 +1,23 @@
 def coach_answer(your_message)
-  # TODO: return coach answer to your_message
+  if your_message == "I am going to work right now!"
+    ""
+  else
+    if your_message.include?("?") == true
+    "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
 end
 
 def coach_answer_enhanced(your_message)
-  # TODO: return coach answer to your_message, with additional custom rules of yours!
+  if your_message.downcase == "i am going to work right now!"
+      ""
+  else
+    if your_message.upcase == your_message
+      "I can feel your motivation! #{coach_answer(your_message)}"
+    else
+      coach_answer(your_message)
+    end
+  end
 end
