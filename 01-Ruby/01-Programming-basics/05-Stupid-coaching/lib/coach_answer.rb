7bd2e1abdfe12d69eddc841644645d5ef228be15
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-14 16:10:58 +0200

    Welcome Rubocop coach

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,23 +1,19 @@
 def coach_answer(your_message)
   if your_message == "I am going to work right now!"
     ""
-  else
-    if your_message.include?("?") == true
+  elsif your_message.include?("?") == true
     "Silly question, get dressed and go to work!"
-    else
-      "I don't care, get dressed and go to work!"
-    end
+  else
+    "I don't care, get dressed and go to work!"
   end
 end
 
 def coach_answer_enhanced(your_message)
   if your_message.downcase == "i am going to work right now!"
-      ""
+    ""
+  elsif your_message.upcase == your_message
+    "I can feel your motivation! #{coach_answer(your_message)}"
   else
-    if your_message.upcase == your_message
-      "I can feel your motivation! #{coach_answer(your_message)}"
-    else
-      coach_answer(your_message)
-    end
+    coach_answer(your_message)
   end
 end
