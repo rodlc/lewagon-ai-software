e36954379e131f5976a52c4e0e91b339adf34186
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-14 18:39:51 +0200

    Pour la gloire

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -1,3 +1,23 @@
 def colorful?(number)
-  # TODO: return true if the number is colorful, false otherwise
+  figure_s = number.to_s.chars
+  figure_i = figure_s.map{|s| s.to_i}
+  if figure_i.count == 1
+    true
+  elsif figure_i.count == 2
+    figure_i << figure_i[0] * figure_i[1]
+    figure_i << figure_i[1] * figure_i[2]
+    if figure_i.uniq.count == figure_i.count
+      true
+    else
+      false
+    end
+  else
+    figure_i << figure_i[0] * figure_i[1]
+    figure_i << figure_i[1] * figure_i[2] * figure_i[3]
+    if figure_i.uniq.count == figure_i.count
+      true
+    else
+      false
+    end
+  end
 end
