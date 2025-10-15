7f05db818397e5d5e94179d90ca66e90ad5edfac
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-15 10:39:45 +0200

    Kudos to Pascal

--- 01-Ruby/02-Flow-Array/02-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Flow-Array/02-Simple-looping/lib/simple_looping.rb
@@ -1,7 +1,27 @@
 def sum_with_for(min, max)
   # CONSTRAINT: you should use a for..end structure
+  if max < min
+    -1
+  else
+    sum = 0
+    for num in min..max
+      sum = sum + num
+    end
+    sum
+  end
 end
 
 def sum_with_while(min, max)
   # CONSTRAINT: you should use a while..end structure
+  if max < min
+    -1
+  else
+    sum = min
+    num = min
+    while num < max
+      num = num + 1
+      sum = sum + num
+    end
+    sum
+  end
 end
