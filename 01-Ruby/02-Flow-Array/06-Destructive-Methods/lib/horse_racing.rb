485b7df875735cc666775dcb761cdb0736e2b028
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-15 14:59:08 +0200

    Belle du jour prends les devants

--- 01-Ruby/02-Flow-Array/06-Destructive-Methods/lib/horse_racing.rb
+++ 01-Ruby/02-Flow-Array/06-Destructive-Methods/lib/horse_racing.rb
@@ -1,3 +1,10 @@
 def horse_racing_format!(race_array)
   # TODO: modify the given array so that it is horse racing consistent. This method should return nil.
+  race_array.each do |horse|
+    horse_index = race_array.index(horse)
+    horse_number = race_array.index(horse) + 1
+    horse_new = horse_number.to_s.concat("-#{horse}!")
+    race_array[horse_index] = horse_new
+  end
+  race_array.sort!.reverse!
 end
