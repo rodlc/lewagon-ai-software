3d3d679da3cb55c10c95cd5533df03874cb56a5b
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-15 13:34:24 +0200

    Black Jack

--- 01-Ruby/02-Flow-Array/04-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Flow-Array/04-Black-Jack/lib/croupier.rb
@@ -2,8 +2,21 @@ require_relative 'black_jack'
 
 def state_of_the_game(player_score, bank_score)
   # TODO: return (not print!) a message containing the player's score and bank's score
+  "Your score is #{player_score}, bank is #{bank_score}"
 end
 
 def end_game_message(player_score, bank_score)
   # TODO: return (not print!) a message telling if the user won or lost.
+  case
+  when player_score > 21
+      "Lose"
+  when player_score == 21
+      "Black Jack"
+  when player_score == bank_score
+      "Push"
+  when player_score > bank_score
+      "You beat the bank! You win."
+  when player_score < bank_score
+      "Bank beat you! You lose."
+  end
 end
--- 01-Ruby/02-Flow-Array/04-Black-Jack/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/04-Black-Jack/lib/interface.rb
@@ -1,4 +1,19 @@
-require_relative "croupier"
+require_relative 'black_jack'
 
-# TODO: make the user play from terminal in a while loop that will stop
-#       when the user will not be asking for  a new card
+def state_of_the_game(player_score, bank_score)
+  "Your score is #{player_score}, bank is #{bank_score}"
+end
+
+def end_game_message(player_score, bank_score)
+  if player_score > 21
+    "Lose"
+  elsif player_score == 21
+    "Black Jack"
+  elsif player_score == bank_score
+    "Push"
+  elsif player_score > bank_score
+    "You beat the bank! You win."
+  elsif player_score < bank_score
+    "Bank beat you! You lose."
+  end
+end
