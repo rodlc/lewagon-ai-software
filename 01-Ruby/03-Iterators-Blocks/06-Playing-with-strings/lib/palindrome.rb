c6c2068ae5964e4a137cb63db9b23f95a1e1e261
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-16 15:31:28 +0200

    Ez

--- 01-Ruby/03-Iterators-Blocks/06-Playing-with-strings/lib/palindrome.rb
+++ 01-Ruby/03-Iterators-Blocks/06-Playing-with-strings/lib/palindrome.rb
@@ -1,3 +1,5 @@
 def palindrome?(a_string)
   # TODO: check if a_string is a palindrome
+  a_string_clean = a_string.gsub(/[^a-zA-Z]/, '').squeeze('').strip.downcase
+  a_string_clean == "" ? false : a_string_clean.reverse == a_string_clean
 end
