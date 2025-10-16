4df845ba909527d60309a366887d70e26ea8d684
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-16 09:41:15 +0200

    Breathe in

--- 01-Ruby/03-Iterators-Blocks/02-Enumerable/lib/enumerable.rb
+++ 01-Ruby/03-Iterators-Blocks/02-Enumerable/lib/enumerable.rb
@@ -1,34 +1,45 @@
 def sum_odd_indexed(array)
   # TODO: computes the sum of elements at odd indexes (1, 3, 5, 7, etc.)
   #       You should use Enumerable#each_with_index
+  sum_odd = 0
+  array.each.with_index do |num, index|
+    sum_odd += num if index.odd?
+  end
+  sum_odd
 end
 
 def even_numbers(array)
   # TODO: Return the even numbers from a list of integers.
   #       You should use Enumerable#select
+  array.select { |num| num.even? }
 end
 
 def short_words(array, max_length)
   # TODO: Take an array of words, return the array of words not exceeding max_length characters
   #       You should use Enumerable#reject
+  array.reject { |word| word.length > max_length }
 end
 
 def first_under(array, limit)
   # TODO: Return the first number from an array that is less than limit.
   #       You should use Enumerable#find
+  array.find { |num| num <= limit }
 end
 
 def add_bang(array)
   # TODO: Take an array of strings and return a new array with "!" appended to each string.
   #       You should use Enumerable#map
+  array.map { |word| "#{word}!" }
 end
 
 def concatenate(array)
   # TODO: Concatenate all strings given in the array.
   #       You should use Enumerable#reduce
+  array.reduce(:+)
 end
 
 def sorted_pairs(array)
   # TODO: Reorganize an array into slices of 2 elements, and sort each slice alphabetically.
   #       You should use Enumerable#each_slice
+  array.each_slice(2).map { |slice| slice.sort }
 end
