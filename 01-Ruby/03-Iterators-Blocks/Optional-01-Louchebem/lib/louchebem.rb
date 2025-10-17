d1ef02e146ca53818a6e91c821720cfdbb349aa1
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-17 10:51:55 +0200

    lansdji losl'oc

--- 01-Ruby/03-Iterators-Blocks/Optional-01-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Iterators-Blocks/Optional-01-Louchebem/lib/louchebem.rb
@@ -1,3 +1,27 @@
 def louchebemize(sentence)
-  # TODO: implement your louchebem translator
+  vowels   = %w[a e i o u y]
+  suffixes = %w[em é ji oc ic uche ès]
+
+  sentence.split.map do |token|
+    first_l = token.index(/\p{L}/)
+    last_l  = token.rindex(/\p{L}/)
+    next token if first_l.nil?
+
+    lead, core, tail = token[0...first_l], token[first_l..last_l], token[(last_l + 1)..]
+    next token if core.length <= 1
+
+    v_idx = core.downcase.chars.index { |c| vowels.include?(c) }
+    trans = if v_idx.nil? || v_idx.zero?
+              "l#{core}#{suffixes.sample}"
+            else
+              cons = core[0...v_idx]
+              rest = core[v_idx..]
+              "l#{rest}#{cons}#{suffixes.sample}"
+            end
+
+    trans[0] = trans[0].upcase if core[0] =~ /[A-Z]/
+    "#{lead}#{trans}#{tail}"
+  end.join(' ')
 end
+
+puts louchebemize("dans l'os")
