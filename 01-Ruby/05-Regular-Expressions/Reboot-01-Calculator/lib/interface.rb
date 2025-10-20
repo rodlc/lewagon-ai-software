024326547ed56ffe3ddedf1db5593f9f069ec0cf
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-20 11:42:28 +0200

    Simple calculator

--- 01-Ruby/05-Regular-Expressions/Reboot-01-Calculator/lib/interface.rb
+++ 01-Ruby/05-Regular-Expressions/Reboot-01-Calculator/lib/interface.rb
@@ -1,1 +1,14 @@
 # TODO: you can build your calculator program here!
+
+require_relative "calculator"
+
+puts "Choisir un 1er nombre"
+first_num = gets.chomp.to_i
+
+puts "Choisir un 2ème nombre"
+second_num = gets.chomp.to_i
+
+puts "Choisir un opérateur entre [+] [-] [*] [/]"
+operator = gets.chomp
+
+puts calculator(first_num, second_num, operator)
