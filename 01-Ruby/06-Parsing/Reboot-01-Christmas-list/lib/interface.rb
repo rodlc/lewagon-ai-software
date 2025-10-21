302afcb75fecc5303aaf66e51d23d52551898376
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-21 15:41:35 +0200

    Happy Christmas

--- 01-Ruby/06-Parsing/Reboot-01-Christmas-list/lib/interface.rb
+++ 01-Ruby/06-Parsing/Reboot-01-Christmas-list/lib/interface.rb
@@ -1,1 +1,36 @@
 # TODO: you can build your christmas list program here!
+
+require_relative 'actions'
+
+p "Welcome to your Christmas gift list!"
+list = [{ name: "Tie", status: false }, { name: "Ruby book", status: false }]
+
+loop do
+	p "Which action [list|add|delete|mark|quit]?"
+	action = gets.chomp
+	case action
+	when "list"
+		if list.empty?
+			p "Empty list!"
+		else
+			list.each do |item|
+				p "#{list.index(item) + 1} - #{item[:status] == true ? "[X]" : "[ ]"} #{item[:name]}"
+			end
+		end 
+	when "add"
+		p "Which item would you like to add?"
+		item = gets.chomp
+		add(list, item)
+	when "delete"
+		p "Which item would you like to delete?"
+		item = gets.chomp
+		delete(list, item)
+	when "mark"
+		p "Which item would have you bought?"
+		item = gets.chomp
+		mark(list, item)
+ 	when "quit"
+		p "Goodbye"
+		break
+	end
+end
