0cd4bbf168a2bd40001f7d3e3e3fc0977b12ab54
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-23 11:53:57 +0200

    Welcome to London mate

--- 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,33 @@
 class Restaurant
   # TODO: add relevant accessors if necessary
+  attr_reader :name, :city, :ratings, :average_rating
 
   def initialize(city, name)
     # TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @ratings = []
+    @average_rating = 0
+  end
+
+  def rate(new_rate)
+    ratings << new_rate
+    @average_rating = ratings.sum.to_f / ratings.size
   end
 
   # TODO: implement .filter_by_city and #rate methods
+  def self.filter_by_city(restaurants, city)
+    city_restaurants = []
+    restaurants.each do |restaurant|
+      city_restaurants << restaurant if restaurant.city == city
+    end
+    city_restaurants
+  end
 end
+
+# jules_verne = Restaurant.new("paris", "Jules Verne")
+# bluebird = Restaurant.new("london", "Bluebird")
+# daniel = Restaurant.new("new york", "Daniel")
+# restaurants = [jules_verne, bluebird, daniel]
+# p Restaurant.filter_by_city(restaurants, "london")
+# => [ #<Restaurant:0x007f9a43bb7eb8 @city="london", @name="Bluebird"> ]
