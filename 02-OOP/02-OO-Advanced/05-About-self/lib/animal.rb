e52d5c32b845258d8d9e6f98ad44ba9eefb37345
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-23 14:48:04 +0200

    Self service

--- 02-OOP/02-OO-Advanced/05-About-self/lib/animal.rb
+++ 02-OOP/02-OO-Advanced/05-About-self/lib/animal.rb
@@ -1,21 +1,27 @@
 class Animal
   def name(name)
     @name = name
+    self
     # TODO: what should this method return?
+    # Name of the instance animal
   end
 
   def species(species)
     @species = species
+    self
     # TODO: what should this method return?
+    #
   end
 
   def color(color)
     @color = color
+    self
     # TODO: what should this method return?
   end
 
   def natural_habitat(natural_habitat)
     @natural_habitat = natural_habitat
+    self
     # TODO: what should this method return?
   end
 
@@ -23,3 +29,5 @@ class Animal
     "Name: #{@name}, Species: #{@species}, Color: #{@color}, Natural Habitat: #{@natural_habitat}"
   end
 end
+
+p Animal.new.name("Fox").color("red").natural_habitat("forest").species("mammal")
