d40068c8403ea1d83fbb5a5743009f20d70eb1be
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-23 11:25:07 +0200

    No farmers no food no future

---
+++ 02-OOP/02-OO-Advanced/02-Farming-Diary-Crops/lib/crop.rb
@@ -0,0 +1,17 @@
+# Add a Crop class to stay DRY
+
+class Crop
+  attr_reader :grains
+
+  def initialize
+    @grains = 0
+  end
+
+  def water!
+    @grains += 10
+  end
+
+  def ripe?
+    @grains >= 15
+  end
+end
--- 02-OOP/02-OO-Advanced/02-Farming-Diary-Crops/lib/farming_diary.rb
+++ 02-OOP/02-OO-Advanced/02-Farming-Diary-Crops/lib/farming_diary.rb
@@ -1,4 +1,3 @@
-# rubocop:disable Lint/LiteralInInterpolation
 # DO NOT remove the two lines below, needed for display purposes
 require_relative "../spec/helper/board"
 
@@ -8,30 +7,28 @@ require_relative "rice"
 
 puts "\n\n📝 Day One: Corn"
 # 1. Instantiate a corn crop
-
+acorn = Corn.new
 # 2. Water the corn crop
-
+acorn.water!
 # 3. Replace 'TODO' with the value of `grains` for your crop
-puts "The corn crop produced #{'TODO'} grains"
+puts "The corn crop produced #{acorn.grains} grains"
 # 4. Replace 'TODO' with the state of the crop ('ripe' or 'not ripe')
-puts "The corn crop is #{'TODO'}"
+puts "The corn crop is #{acorn.grains}"
 
 
 
 puts "\n\n📝 Day Two: Rice"
 # 1. Instantiate a rice crop
-
+ricey = Rice.new
 # 2. Water the rice crop
-
+ricey.water!
 # 3. Transplant the rice crop
-
+ricey.transplant!
 # 4. Replace 'TODO' with the value of `grains` for your crop
-puts "The rice crop produced #{'TODO'} grains"
+puts "The rice crop produced #{ricey.grains} grains"
 # 5. Replace 'TODO' with the state of the crop ('ripe' or 'not ripe')
-puts "The rice crop is #{'TODO'}"
+puts "The rice crop is #{ricey.grains}"
 
 
 # DO NOT remove the two lines below, needed for display purposes
 Board.new.display
-
-# rubocop:enable Lint/LiteralInInterpolation
