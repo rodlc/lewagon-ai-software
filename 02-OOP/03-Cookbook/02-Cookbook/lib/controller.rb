88f5121ebd287e72d00f7ccdb0d9d67463053f54
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-24 14:36:35 +0200

    First MVC \o/

--- 02-OOP/03-Cookbook/02-Cookbook/lib/controller.rb
+++ 02-OOP/03-Cookbook/02-Cookbook/lib/controller.rb
@@ -1,1 +1,28 @@
 #  TODO: Define your Controller Class here, to orchestrate the other classes
+
+require_relative "cookbook"
+require_relative "view"
+
+class Controller
+
+  def initialize(cookbook)
+    @cookbook = cookbook
+  end
+
+  def list
+    @cookbook.all
+    View.new.display_recipes(@cookbook.all)
+  end
+
+  def add
+  	recipe_name = View.new.ask_for("nom de la recette")
+    recipe_description = View.new.ask_for("description de la recette")
+    @cookbook.create(recipe_name, recipe_description)
+  end
+
+  def remove
+  	index = View.new.ask_for_index
+    @cookbook.destroy(index)
+  end
+
+end
--- 02-OOP/03-Cookbook/02-Cookbook/lib/cookbook.rb
+++ 02-OOP/03-Cookbook/02-Cookbook/lib/cookbook.rb
@@ -1,1 +1,22 @@
 # TODO: Implement the Cookbook class that will be our repository
+
+require_relative "recipe"
+
+class Cookbook
+  def initialize
+    @recipes = []
+  end
+
+  def all
+    @recipes
+  end
+
+  def create(recipe_name, recipe_description)
+    recipe = Recipe.new(recipe_name, recipe_description)
+    @recipes << recipe
+  end
+
+  def destroy(recipe_index)
+    @recipes.delete_at(recipe_index)
+  end
+end
