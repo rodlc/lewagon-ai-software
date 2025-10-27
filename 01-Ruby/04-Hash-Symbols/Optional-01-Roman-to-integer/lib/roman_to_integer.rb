ac9f86aed7fc613d23077193134a4e1473263244
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-27 19:25:02 +0100

    Roman to arabic numerals

--- 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
@@ -1,3 +1,17 @@
+ROMAN_VALUES = {
+  "I" => 1, "V" => 5, "X" => 10, "L" => 50,
+  "C" => 100, "D" => 500, "M" => 1000
+}
+
 def roman_to_integer(roman_string)
-  # TODO: translate roman string to integer
+  chars = roman_string.upcase.chars
+  return 0 unless chars.all? { |char| ROMAN_VALUES.key?(char) }
+
+  total = previous_value = 0
+  chars.reverse_each do |char|
+    current_value = ROMAN_VALUES[char]
+    total += current_value < previous_value ? -current_value : current_value
+    previous_value = current_value
+  end
+  total
 end
