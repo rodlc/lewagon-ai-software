9df7097afe10fd6d251133a6b0fac6b41bcb686c
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-27 16:24:52 +0100

    Too shy a shy

--- 01-Ruby/04-Hash-Symbols/04-Argument-order-dependency/lib/refrain.rb
+++ 01-Ruby/04-Hash-Symbols/04-Argument-order-dependency/lib/refrain.rb
@@ -13,14 +13,14 @@ end
 def better_refrain(lyrics, options = { vibrato: 0, number_of_times: 1, strong: false })
   # TODO: implement this better version which breaks argument order dependency
   song_refrain = []
-  lyrics += lyrics[:number_of_times] * options[:vibrato]
+  lyrics += lyrics[lyrics.size - 1] * options[:vibrato]
   lyrics.upcase! if options[:strong]
 
-  number_of_times.times do
+  options[:number_of_times].times do
     song_refrain << lyrics
   end
 
   song_refrain.join(" ")
 end
 
-puts better_refrain("Too shy", options = { vibrato: 0, number_of_times: 1, strong: false })
+puts better_refrain("Too shy", { vibrato: 0, number_of_times: 10, strong: true })
