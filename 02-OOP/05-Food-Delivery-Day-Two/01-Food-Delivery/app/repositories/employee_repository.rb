b9e0d9495ea514975e8da8949c722e72ab970ee9
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-28 10:17:56 +0100

    Employee repository

---
+++ 02-OOP/05-Food-Delivery-Day-Two/01-Food-Delivery/app/repositories/employee_repository.rb
@@ -0,0 +1,49 @@
+require_relative "../models/employee"
+require "csv"
+
+class EmployeeRepository
+  attr_accessor :next_id
+
+  def initialize(csv_file_path)
+    @csv_file_path = csv_file_path
+    @employees = []
+    @next_id = 1
+    load_csv
+  end
+
+  def all_riders
+    @employees.select { |rider| rider.rider? }
+  end
+
+  def find(id)
+    @employees[id - 1]
+  end
+
+  def find_by_username(username)
+    @employees.select { |employee| employee.username == username }[0]
+  end
+
+  private
+
+  def load_csv
+    return unless File.exist?(@csv_file_path)
+
+    CSV.foreach(@csv_file_path, headers: true, header_converters: :symbol) do |row|
+      row[:id] = row[:id].to_i
+      row[:username] = row[:username].to_s
+      row[:password] = row[:password].to_s
+      row[:role] = row[:role].to_s
+      @employees << Employee.new(row)
+    end
+    @next_id = @employees.empty? ? 1 : @employees.last.id + 1
+  end
+
+  def save_csv
+    CSV.open(@csv_file_path, "wb") do |csv|
+      csv << ["id", "username", "password", "role"]
+      @employees.each do |employee|
+        csv << [employee.id, employee.username, employee.password, employee.role]
+      end
+    end
+  end
+end
