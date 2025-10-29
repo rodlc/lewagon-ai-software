99a24d4b05868ff3520948349b3a55f080c05826
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-29 18:58:07 +0100

    Ting ting

--- 02-OOP/01-OO-Basics/04-Public-Interface/lib/bank_account.rb
+++ 02-OOP/01-OO-Basics/04-Public-Interface/lib/bank_account.rb
@@ -10,6 +10,7 @@ class BankAccount
   # - you can print transactions only with a password
   # - you can withdraw or deposit money
   # - You can see the balance of the account (through the balance variable)
+  attr_reader :name, :balance
 
   MIN_DEPOSIT = 100
 
@@ -28,25 +29,39 @@ class BankAccount
   def withdraw(amount)
     # TODO: Call add_transaction with the right argument
     # TODO: returns a string with a message
+    add_transaction(-amount)
+    "Withdrawal completed! Withdrawn amount: #{amount}"
   end
 
   def deposit(amount)
     # TODO: Call add_transaction with the right argument
     # TODO: returns a string with a message
+    add_transaction(amount)
+    "Deposit completed! Deposited amount: #{amount}"
   end
 
   def transactions_history(args = {})
     # TODO: Check if there is a password and if so if it is correct
     # TODO: return a string displaying the transactions, BUT NOT return the transaction array!
+    if args.empty?
+      "no password given"
+    elsif args[:password] != @password
+      "wrong password"
+    else
+      @transactions.join("\n")
+    end
   end
 
   def iban
     # TODO: Hide the middle of the IBAN like FR14**************606 and return it
+    iban_raw = @iban.gsub("-", "").chars
+    iban_raw.map.with_index { |char, index| index <= 3 || index >= iban_raw.count - 3 ? char : "*" }.join
   end
 
   def to_s
     # Method used when printing account object as string (also used for string interpolation)
     # TODO: return a string with the account owner, the hidden iban and the balance of the account
+    "Owner: #{@name} - IBAN: #{iban} - Balance: #{@balance}"
   end
 
   private
@@ -54,5 +69,10 @@ class BankAccount
   def add_transaction(amount)
     # TODO: add the amount in the transactions array
     # TODO: update the current balance (which represents the balance of the account)
+    @transactions << amount
+    @balance += amount
   end
 end
+
+account = BankAccount.new("John Lennon", "FR14-2004-1010-0505-0001-3M02-606", 200, "yoko")
+p account.transactions_history({ password: "yoko" })
