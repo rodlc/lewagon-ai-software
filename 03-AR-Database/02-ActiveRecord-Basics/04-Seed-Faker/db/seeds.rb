4d6b95ae015daa28119ec270d1ecdcffebab0c18
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-30 11:58:07 +0100

    Faker Posts

--- 03-AR-Database/02-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/02-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
@@ -1,2 +1,5 @@
 require "faker"
 # TODO: Write a seed to insert 100 posts in the database
+100.times do |_i|
+  Post.create(title: Faker::Music.band, url: Faker::Sports::Football.player, votes: 2)
+end
