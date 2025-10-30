5557f02bb8f8c41c4655b300b98f4657654ba046
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-30 09:58:04 +0100

    DB Migration

--- 03-AR-Database/02-ActiveRecord-Basics/02-Migrations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/02-ActiveRecord-Basics/02-Migrations/db/migrate/20141025152200_create_posts.rb
@@ -1,5 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     # TODO: your code here to create the posts table
+    create_table :posts do |t|
+      t.string :title
+      t.string :url
+      t.integer :votes, default: 0
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
   end
 end
