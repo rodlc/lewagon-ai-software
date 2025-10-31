59aaefb124e2e9ac2666148db23aff71a5009c3c
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-31 11:32:04 +0100

    Validations & Callbacks

--- 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -1,5 +1,11 @@
+require "active_record"
+
 class Post < ActiveRecord::Base
   belongs_to :user
 
   # TODO: Add some validation
+  validates :user, presence: true
+  validates :title, presence: true, length: { minimum: 5 }, uniqueness: { case_sensitive: false }
+  validates :url, presence: true,
+                  format: { with: /\A(http|https):\/\/[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix }
 end
