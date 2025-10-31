b8f600e323e02eb80903f271677507d03eb555d7
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-10-31 11:34:46 +0100

    Good style

--- 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/03-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ActiveRecord::Base
   # TODO: Add some validation
   validates :user, presence: true
   validates :title, presence: true, length: { minimum: 5 }, uniqueness: { case_sensitive: false }
-  validates :url, presence: true,
-                  format: { with: /\A(http|https):\/\/[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix }
+  validates :url,
+            presence: true,
+            format: { with: /\A(http|https):\/\/[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix }
 end
