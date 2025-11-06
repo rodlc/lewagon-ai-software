fa720c8d16bf985e0045bd8cdc65bb9d1bb7a09b
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-06 11:47:43 +0100

    JS Select

--- 04-Front-End/04-JS-and-DOM/02-Select-element/lib/select.js
+++ 04-Front-End/04-JS-and-DOM/02-Select-element/lib/select.js
@@ -5,10 +5,11 @@
 import runChallenges from "../spec/select_examiner.js";
 
 // TODO 1. Open the `index.html` file and set an id on the France <li>
-
 const franceElement = () => {
   // TODO 2: select the France list item and return it!
+  const element = document.getElementById("one");
+  return element;
 };
 
-// DO NOT MODIFY THIS LINE. It checks your answer.
+// DO NOT MODIFY   THIS LINE. It checks your answer.
 runChallenges(franceElement);
