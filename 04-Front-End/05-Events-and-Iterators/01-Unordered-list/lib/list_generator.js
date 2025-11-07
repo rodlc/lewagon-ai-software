bf95bf9c86997e9a07d5441c8bf271e4ae5540bd
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-07 10:41:18 +0100

    Groceries list

--- 04-Front-End/05-Events-and-Iterators/01-Unordered-list/lib/list_generator.js
+++ 04-Front-End/05-Events-and-Iterators/01-Unordered-list/lib/list_generator.js
@@ -6,10 +6,16 @@ import runChallenges from "../spec/list_generator_examiner.js";
 
 const listItem = (content) => {
   // TODO: return the proper <li> HTML tag with its content (as a string)
+  return `<li class="list-group-item">${content}</li>`;
 };
 
 const unorderedList = (items) => {
   // TODO: return the proper <ul> markup (as a string)
+  let itemsList = [];
+  items.forEach((item) => {
+    itemsList += listItem(item);
+  });
+  return `<ul class="list-group">${itemsList}</ul>`;
 };
 
 // Do not remove these lines:
@@ -17,6 +23,8 @@ if (typeof window === "object") {
   document.addEventListener("DOMContentLoaded", () => {
     // TODO: call unorderedList() passing the right element
     // TODO: display the list in the browser
+    const list = document.querySelector("#list");
+    list.insertAdjacentHTML("beforeend", unorderedList(['milk', 'butter', 'bread']));
   });
 }
 
