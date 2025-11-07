962a1cd944c8533aec859521584367016972c8c0
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-07 10:08:19 +0100

    Bingo!

--- 04-Front-End/05-Events-and-Iterators/02-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/05-Events-and-Iterators/02-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,8 @@
 // TODO: React to a click on the button!
+const button = document.getElementById("clickme");
+
+button.addEventListener("click", (event) => {
+  button.classList.add("disabled");
+  button.textContent = 'Bingo!';
+  return button;
+});
