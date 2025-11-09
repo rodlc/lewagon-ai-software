e319217ae708b304b663ccdfeed555f597676452
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-09 14:36:10 +0100

    Comparables

--- 02-OOP/01-OO-Basics/Optional-01-Richest-Student/lib/student.rb
+++ 02-OOP/01-OO-Basics/Optional-01-Richest-Student/lib/student.rb
@@ -1,2 +1,24 @@
 class Student
+  include Comparable
+
+  attr_reader :name, :fives, :tens, :twenties
+
+  def <=>(other)
+    self.wealth <=> other.wealth
+  end
+
+  def initialize(name, fives, tens, twenties)
+    @name = name
+    @fives = fives
+    @tens = tens
+    @twenties = twenties
+  end
+
+  def wealth
+    (@fives * 5) + (@tens * 10) + (@twenties * 20)
+  end
 end
+
+# p alice = Student.new("Alice", 1, 2 ,3)
+# p paul = Student.new("Paul", 3, 2 ,1)
+# p alice.wealth
