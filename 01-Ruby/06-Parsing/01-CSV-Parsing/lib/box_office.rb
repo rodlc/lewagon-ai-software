2fbe81d08501e6a0767361f94ff98595f6ebc855
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-10 22:38:44 +0100

    Refactor o3

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -1,22 +1,13 @@
-require 'csv'
-require 'date'
+require "csv"
 
-def most_successful(number, max_year, file_path)
-  # TODO: return an array of most successful movies before `max_year`
-  movies_selection = []
-  movies_sorted = []
-  CSV.foreach(file_path) do |row|
-    unless row[1].to_i >= max_year
-      movie = {}
-      movie[:name] = row[0]
-      movie[:year] = row[1].to_i
-      movie[:earnings] = row[2].to_i
-      movies_selection << movie
-    end
+def most_successful(limit, max_year, file_path)
+  movies = CSV.foreach(file_path).with_object([]) do |(name, year, earnings), list|
+    next if year.to_i >= max_year
+
+    list << { name: name,
+              year: year.to_i,
+              earnings: earnings.to_i }
   end
-  puts movies_sorted
-  movies_sorted = movies_selection.sort_by { |movie| movie[:earnings] }.reverse
-  movies_sorted[0..(number - 1)]
-end
 
-puts most_successful(6, 1975, "./lib/movies.csv")
+  movies.max_by(limit) { |m| m[:earnings] } # top-N sans trier tout le tableau
+end
