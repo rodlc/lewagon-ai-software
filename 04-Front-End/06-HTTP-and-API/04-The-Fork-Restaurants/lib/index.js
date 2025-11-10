9ee87aee625a6fd8459e18495cc1483dbe6f5a3e
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-10 16:36:28 +0100

    Fork API

--- 04-Front-End/06-HTTP-and-API/04-The-Fork-Restaurants/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/04-The-Fork-Restaurants/lib/index.js
@@ -5,16 +5,30 @@
 const searchForm = document.getElementById("searchForm");
 
 // Todo: select the HTML elements you need
-
+const restaurantsList = document.getElementById("restaurant-list");
 
 searchForm.addEventListener("submit", (event) => {
   // Todo: Find the category selected and build the URL you will send your request to
+  restaurantsList.innerHTML = "";
+  event.preventDefault();
+  const userCategory = document.querySelector('input[name="category"]:checked').value;
+  const userLocation = document.getElementById('location').value;
+
+  let url;
+  if (userLocation) {
+    url = `https://the-fork.api.lewagon.com/api/v1/restaurants?category=${userCategory}&location=${userLocation}`;
+  } else {
+    url = `https://the-fork.api.lewagon.com/api/v1/restaurants?category=${userCategory}`;
+  }
 
   // Todo: Replace "the-endpoint-url" with the URL you built
-  fetch("the-endpoint-url")
+  fetch(`${url}`)
     .then(response => response.json())
     .then((data) => {
       // Todo: Insert results into the list
-
+      console.log(data);
+      data.forEach((restaurant) => {
+        restaurantsList.insertAdjacentHTML(`beforeend`, `<li class="list-group-item">${restaurant.name} in ${restaurant.address}</li>`);
+      });
     });
 });
