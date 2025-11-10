a381425fcabd729eedab6085eff792891133d265
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-10 11:32:31 +0100

    Clicked elements

--- 04-Front-End/06-HTTP-and-API/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,8 @@
 // TODO: write your code here!
+const cards = document.querySelectorAll(".clickable");
+
+cards.forEach((card) => {
+  card.addEventListener("click", (event) => {
+    card.classList.toggle("active");
+  });
+});
