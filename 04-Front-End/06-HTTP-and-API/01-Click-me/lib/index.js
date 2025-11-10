a0ae60705e9d5c29a622827717ea89a0d64da9bc
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-10 11:07:51 +0100

    Click me!

--- 04-Front-End/06-HTTP-and-API/01-Click-me/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/01-Click-me/lib/index.js
@@ -2,6 +2,11 @@ const displayAlertOnButtonClick = () => {
   // TODO: Select the big green button
   // TODO: Bind the `click` event to the button
   // TODO: On click, display `Thank you!` in a JavaScript alert!
+  const button = document.querySelector(".btn.btn-lg.btn-success");
+
+  button.addEventListener("click", (event) => {
+    button.insertAdjacentHTML("afterend", "<p>Thank you!</p>");
+  });
 };
 
 displayAlertOnButtonClick(); // Do not remove!
