17e0e5b42fe5adb5a785f1b718a8f8a8b3283f6c
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-11 09:52:32 +0100

    Dictionnary API

--- 04-Front-End/07-AJAX/01-Name-Age/lib/index.js
+++ 04-Front-End/07-AJAX/01-Name-Age/lib/index.js
@@ -1,10 +1,17 @@
 const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const pageDefinition = document.getElementById("definition");
 
 const displayDefinition = (event) => {
   event.preventDefault();
   const word = document.getElementById("word").value;
   // TODO: Display your word's definition with an AJAX call instead of the console.log()
-  console.log(word);
+  fetch(`${apiUrl}${word}`)
+    .then(response => response.json())
+    .then((data) => {
+      // Todo: Insert results into the list
+      const firstDefinition = data[0].meanings[0].definitions[0].definition;
+      pageDefinition.innerText = firstDefinition;
+    });
 };
 
 const form = document.getElementById("fetch-word");
