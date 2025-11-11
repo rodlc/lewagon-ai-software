d62c52c74c65119150bec40a085e9d1eb1d60b13
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-11 11:34:48 +0100

    Google

--- 04-Front-End/07-AJAX/02-AJAX-Autocomplete/lib/index.js
+++ 04-Front-End/07-AJAX/02-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,25 @@
 // TODO: Autocomplete the input with AJAX calls.
+
+const apiUrl = "https://dictionary.lewagon.com/autocomplete/";
+const listElement = document.getElementById("results");
+const search = document.getElementById("search");
+
+const autoComplete = (event) => {
+  if (!search.value) return;
+  listElement.textContent = "";
+  const stem = document.getElementById("search").value;
+  // event.preventDefault();
+
+  // TODO: Display your word's definition with an AJAX call instead of the console.log()
+  fetch(`${apiUrl}${stem}`)
+    .then(response => response.json())
+    .then((data) => {
+      // Todo: Insert results into the list
+      const firstCompletions = Array.from(data.words).slice(0, 5);
+      firstCompletions.forEach((word) => {
+        listElement.insertAdjacentHTML("beforeend", `<li class="list-group-item"> ${word}</li>`);
+      });
+    });
+};
+
+search.addEventListener("keyup", autoComplete);
