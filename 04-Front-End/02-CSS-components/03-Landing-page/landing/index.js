2071e78edf5ad76989aba134a2b1c0454a784865
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-12 14:36:37 +0100

    JS Button?

--- 04-Front-End/02-CSS-components/03-Landing-page/landing/index.js
+++ 04-Front-End/02-CSS-components/03-Landing-page/landing/index.js
@@ -1,8 +1,9 @@
 const displayAlertOnButtonClick = () => {
-  
+
   const button = document.querySelector(".btn-primary");
 
   button.addEventListener("click", (event) => {
+    preventDefault();
     button.innerText = "Coming soon!";
   });
 };
