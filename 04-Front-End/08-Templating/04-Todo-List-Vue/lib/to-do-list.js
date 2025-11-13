c189495c3be8a0939073961cd3dcd2119c480fe4
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-13 16:25:50 +0100

    Vue.js

--- 04-Front-End/08-Templating/04-Todo-List-Vue/lib/to-do-list.js
+++ 04-Front-End/08-Templating/04-Todo-List-Vue/lib/to-do-list.js
@@ -3,7 +3,14 @@ import { createApp } from "vue";
 createApp({
   data() {
     return {
-      message: "If you see this message in your browser, that means Vue is successfully mounted! 🙌"
+      message: "If you see this message in your browser, that means Vue is successfully mounted! 🙌",
+      todos: [
+        { title: "Code a to-do list", done: false },
+        { title: "Eat breakfast", done: true },
+        { title: "Do some exercise", done: false },
+        { title: "Water the plants", done: true }
+      ]
     };
   }
 }).mount('#todosContainer');
+  
