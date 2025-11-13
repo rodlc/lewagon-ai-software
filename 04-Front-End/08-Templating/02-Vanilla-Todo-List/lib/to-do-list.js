36ada2c0c3e20b3da89dd7bbe493909ddb108782
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-13 12:13:50 +0100

    JS Template

--- 04-Front-End/08-Templating/02-Vanilla-Todo-List/lib/to-do-list.js
+++ 04-Front-End/08-Templating/02-Vanilla-Todo-List/lib/to-do-list.js
@@ -2,7 +2,22 @@ const todos = [
   { title: "Code a to-do list", done: false },
   { title: "Eat breakfast", done: true },
   { title: "Do some exercise", done: false },
-  { title: "Water the plants", done: true }
+  { title: "Water the plants", done: false }
 ];
 
 // TODO: Use the template in the `index.html` to dynamically generate a list based on `todos` array
+document.addEventListener("DOMContentLoaded", () => {
+  // 1) références utiles
+  const template = document.querySelector("#todoItemTemplate");
+  const results = document.getElementById("todosContainer");
+
+  // 2) rendu
+  todos.forEach((item) => {
+    const clone = template.content.cloneNode(true);
+
+    clone.querySelector(".title").textContent = item.title;
+    clone.querySelector("input[type=checkbox]").checked = item.done;
+
+    results.appendChild(clone);
+  });
+});
