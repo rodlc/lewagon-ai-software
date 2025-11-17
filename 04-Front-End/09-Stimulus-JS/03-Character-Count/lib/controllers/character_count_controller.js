f6541a6fd9ab7777f4205b08ec5187f778116f69
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-17 11:17:17 +0100

    Character count

---
+++ 04-Front-End/09-Stimulus-JS/03-Character-Count/lib/controllers/character_count_controller.js
@@ -0,0 +1,9 @@
+// lib/controllers/character_count_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    // TODO: console.log something!
+    console.log("Hello world!")
+  }
+}
--- 04-Front-End/09-Stimulus-JS/03-Character-Count/lib/index.js
+++ 04-Front-End/09-Stimulus-JS/03-Character-Count/lib/index.js
@@ -2,6 +2,8 @@
 import { Application } from "@hotwired/stimulus";
 
 // TODO: Import the Stimulus Controller here
+import CharacterCountController from "./controllers/character_count_controller.js"
 
 window.Stimulus = Application.start();
 // TODO: Register your Stimulus Controller below
+Stimulus.register("character-count", CharacterCountController)
