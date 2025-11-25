da8303cb34fd05c6acf75de033b0de767c48723d
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-25 08:15:58 +0100

    Longest word

--- 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
+++ 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
@@ -2,9 +2,44 @@ require 'open-uri'
 require 'json'
 
 def generate_grid(grid_size)
-  # TODO: generate random grid of letters
+  Array.new(grid_size) { ('A'..'Z').to_a.sample }
+end
+
+def included?(attempt, grid)
+  attempt.upcase.chars.all? { |letter| attempt.upcase.count(letter) <= grid.count(letter) }
+end
+
+def english_word?(word)
+  response = URI.open("https://dictionary.lewagon.com/#{word}")
+  json = JSON.parse(response.read)
+  json['found']
+end
+
+def score_and_message(attempt, grid, time)
+  if included?(attempt, grid)
+    if english_word?(attempt)
+      score = compute_score(attempt, time)
+      [score, "Well done!"]
+    else
+      [0, "not an english word"]
+    end
+  else
+    [0, "not in the grid"]
+  end
+end
+
+def compute_score(attempt, time_taken)
+  # Arbitrary scoring rule: lengths over time
+  time_taken > 60.0 ? 0 : attempt.size * (1.0 - time_taken / 60.0)
 end
 
 def run_game(attempt, grid, start_time, end_time)
-  # TODO: runs the game and return detailed hash of result (with `:score`, `:message` and `:time` keys)
+  result = { time: end_time - start_time }
+
+  score, message = score_and_message(attempt, grid, result[:time])
+  
+  result[:score] = score
+  result[:message] = message
+
+  return result
 end
